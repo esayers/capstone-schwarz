0e023c7c4e58d1470364a2ae281ab6736c759969
Ben Wilson <btw@pdx.edu>
2015-03-04 21:07:59 -0800

    Revision 2 of readCaptureData.m
    Added .xml parser function
    Added option to pass arguments to readCaptureData
    Time domain plot removed
    Elapsed time added to frequency domain plot
    See program header for full info on updates

--- Matlab/readCaptureData.m
+++ Matlab/readCaptureData.m
@@ -1,95 +1,124 @@
+function readCaptureData(varargin)
 % Author: Ben Wilson
 % Date:   February, 25, 2015
+% Revision: 2 - March, 4, 2015
+%     xml parser function to pull data from .xml files
+%     option to pass file name and sample number to the function
+%     elapsed time added to frequency domain plot
+%	  removed time domain plot for increased plotting speed
+%	  figure opens in full screen
+%	  
+% Requires: 
+%     parseXML.m
+%     procFFT.m
+%     procIQData.m
 % ------------------------------------------------------------------------
 % This program reads IQ data from a binary file and plots the results in
-% the time and frequency domain.
+% the frequency domain.
 %
 % In order to run this script, you will need to untar the binary file using
-% the matlab function: "untar(tarfilename,outputdir)". Once the binary file
-% is in the same directory as this script, you can enter the file name into
-% line 17 and run the program.
-%
-% The values of the sampling frequency, numer of samples, and other
-% metadata can be found the xml file that accompanied the binary file.
+% the matlab function: "untar(tarfilename,outputdir)". Binary files and
+% .xml files must be in subdirectories of this script.
+% 
+% readCaptureData(XMLfile) - pass the .xml file as a string to the 
+% function. Assumes that the number of sample points for the FFT is 2^16.
+% 
+% readCaptureData(XMLfile, powerOfTwo) - pass the .xml file as a string and
+% the number of sample points for the FFT in terms of the power of two as 
+% an integer. 
 %-------------------------------------------------------------------------
-clear;clc
 
-biFileName = 'C:\Users\wilson_b\Desktop\Capstone\Jammer Captures\Jammer files from CO\Jammer 2\Jammer 2 200 ms at 62.5MSa\File_00TT.complex.1ch.float32';
-fID = fopen(biFileName);  % Open the binary file
-prec = 'single';
+if nargin == 1
+    xmlFileName = varargin{1};
+    numPoint = 2^16;
+elseif nargin == 2
+    xmlFileName = varargin{1};
+    numPoint = 2^varargin{2};
+else
+    disp('incorrect number of input arguments');
+    return
+end
 
-numPoint = 2^16;        % Enter a power of 2
-samples = 12500000;     % Numbers of samples
-fs= 6.25e+007;          % Sampling frequency
-centerFreq = 1.57542e9; % Center frequency
-T = 1/fs;               % Sample period
-t = (0:numPoint-1)*T;	% Time vector
-f = centerFreq + fs/2*linspace(-1,1,numPoint);
+addpath(genpath(pwd))
+disp('Attempting to parse .xml file...')
+xDoc = parseXML(xmlFileName);
+disp('    ...parsing complete.')
 
-% Set axis limits of the first plot
-xmin1 = 0;
-xmax1 = 200e-3;
-ymin1 =  1e6;
-ymax1 = -1e6;
-% Set axis limits of the second plot
-xmin2 = centerFreq - fs/2;
-xmax2 = centerFreq + fs/2;
-ymin2 =  1e6;
-ymax2 = -1e6;
+numSamples= str2double(xDoc(3).Children( 8).Children.Data);
+sampRate  =	str2double(xDoc(3).Children(10).Children.Data);
+centerFreq= str2double(xDoc(3).Children(22).Children.Children.Children.Children.Data);
+dataType  =	xDoc(3).Children(14).Children.Data;
+fileName  =	xDoc(3).Children(20).Children.Data;
+fID = fopen(fileName);  % Open the binary file
+% scalFact  =	str2double(xDoc(3).Children(16).Children.Data);
+% format    =	xDoc(3).Children(12).Children.Data;
 
-
+% duration  = numSamples/sampRate;
+sampPeriod = 1/sampRate;
+t = (0:numPoint-1)*sampPeriod;                       % Time axis array
+f = centerFreq + sampRate/2*linspace(-1,1,numPoint); % Frequency axis array
 
 % Create the figure and define two subplots
-figure(1)
-ax1 = subplot(2,1,1); % first subplot
-ax2 = subplot(2,1,2); % second subplot
-title (ax1,'Power vs. Time')
-xlabel(ax1,'Time (S)')
-ylabel(ax1,'Power (dBm)')
-grid  (ax1, 'on');
-hold(ax1, 'on')
+figure('units','normalized','outerposition',[0 0 1 1])
+% ax1 = subplot(2,1,1); % first subplot
+ax2 = subplot(1,1,1); % second subplot
+% title (ax1,'Power vs. Time')
+% xlabel(ax1,'Time (S)')
+% ylabel(ax1,'Power (dBm)')
+% grid  (ax1, 'on');
+% hold(ax1, 'on')
 
-i = floor(samples/numPoint);    % Number of plot iterations
-% clear biFileName samples fs centerFreq xmin1 xmax1 ymin1 ymax1
+i = floor(numSamples/numPoint);    % Number of plot iterations
+IQraw = zeros(i, numPoint);
+IQdBm = zeros(i, numPoint);
+spec = zeros(i, numPoint);
+
+clear fileName numSamples xDoc xmlFileName
+disp('Loading IQ data into MatLab...')
 for n = 1:i
     % Process data from binary file
-    [IQraw, ~, IQdBm, ~] = procIQData(fID, numPoint, prec);
+    [IQraw(n,:), ~, IQdBm(n,:), ~] = procIQData(fID, numPoint, dataType);
     
     % Compute the FFT
-    spec = procFFT(IQraw, numPoint);
-    
-    if max(IQdBm) > ymax1
-        ymax1 = 5*ceil(max(IQdBm)/5);
-    end
-    
-    if min(IQdBm) < ymin1
-        ymin1 = 5*floor(min(IQdBm)/5);
-    end
-    
-    if max(spec) > ymax2
-        ymax2 = 5*ceil(max(spec)/5);
-    end
-    
-    if min(spec) < ymin2
-        ymin2 = 5*floor(min(spec)/5);
-    end
-    
-    plot(ax1, t, IQdBm,'b')     % Plot the time domain plot
-    axis(ax1, [xmin1 xmax1 ymin1 ymax1])
-    plot(ax2, f, spec, 'r')     % Plot the FFT plot
+    spec(n,:) = procFFT(IQraw(n,:), numPoint);
+end
+disp('    ...complete.')
+% Set axis limits of the first plot
+% xmin1 = 0;
+% xmax1 = duration;
+% ymin1 = 5*floor(min(IQdBm(:))/5);
+% ymax1 = 5*ceil(max(IQdBm(:))/5);
+
+% Set axis limits of the second plot
+xmin2 = centerFreq - sampRate/2;
+xmax2 = centerFreq + sampRate/2;
+ymin2 = 5*floor(min(spec(:))/5)+10;
+ymax2 = 5*ceil(max(spec(:))/5);
+
+clear centerFreq dataType sampRate
+
+for n = 1:i
+%     plot(ax1, t, IQdBm(n,:),'b')     % Plot the time domain plot
+%     axis(ax1, [xmin1 xmax1 ymin1 ymax1])
+    plot(ax2, f, spec(n,:), 'r')     % Plot the FFT plot
     title (ax2,'Power vs. Frequency')
+    text(xmin2, ymin2+10, ['  Elapsed Time: ', num2str(t(end)), ' s'])
     xlabel(ax2,'Frequency (Hz)')
     ylabel(ax2,'Power (dBm)')
     axis(ax2, [xmin2 xmax2 ymin2 ymax2])
     grid(ax2, 'on');
     
-    t = t+(numPoint*T);
+    t = t+(numPoint*sampPeriod);
     pause(.01);
 end
+
 status = fclose(fID);    % Close the binary file
 if ~status
-    disp('File closed: exiting program');
+    disp('File closed:');
+    x = input('Press enter to close the program');
 else
     disp('Error closing file!');
 end
 close;
+clear;clc
+end

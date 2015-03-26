001fe1ae8428ba5a77552fd2605239343ac7ecab
Ben Wilson <btw@pdx.edu>
2015-03-25 23:38:23 -0700

    Added BW detection and chirp rate calculations
    Along with updating the variables to more intuitive names, I created a
    bandwidth detection algorithm that takes smaller FFTs to determine the
    jammer's sweep rate and bandwidth.

--- Matlab/readCaptureData.m
+++ Matlab/readCaptureData.m
@@ -1,17 +1,18 @@
 function readCaptureData(varargin)
 % Author: Ben Wilson
-% Date:   February, 25, 2015
-% Revision: 2 - March, 4, 2015
-%     xml parser function to pull data from .xml files
-%     option to pass file name and sample number to the function
-%     elapsed time added to frequency domain plot
-%	  removed time domain plot for increased plotting speed
-%	  figure opens in full screen
-%	  
-% Requires: 
+% Date:   February 25, 2015
+% Revision: 2 - March  4, 2015
+%     add the xml parser function to pull data from .xml files
+%     add the option to pass file name and sample number to the function
+% Revision: 3 - March 25, 2015
+%     bandwith calculation added
+%     chirp rate calculation added
+%     updated variable names to be more descriptive
+% Requires:
 %     parseXML.m
 %     procFFT.m
 %     procIQData.m
+%     prettyUnits.m
 % ------------------------------------------------------------------------
 % This program reads IQ data from a binary file and plots the results in
 % the frequency domain.
@@ -19,106 +20,131 @@ function readCaptureData(varargin)
 % In order to run this script, you will need to untar the binary file using
 % the matlab function: "untar(tarfilename,outputdir)". Binary files and
 % .xml files must be in subdirectories of this script.
-% 
-% readCaptureData(XMLfile) - pass the .xml file as a string to the 
+%
+% readCaptureData(XMLfile) - pass the .xml file as a string to the
 % function. Assumes that the number of sample points for the FFT is 2^16.
-% 
+%
 % readCaptureData(XMLfile, powerOfTwo) - pass the .xml file as a string and
-% the number of sample points for the FFT in terms of the power of two as 
-% an integer. 
+% the number of sample points for the FFT in terms of the power of two as
+% an integer.
 %-------------------------------------------------------------------------
 
 if nargin == 1
-    xmlFileName = varargin{1};
-    numPoint = 2^16;
+    xml_file_name = varargin{1};
+    num_fft_pts = 2^16;
 elseif nargin == 2
-    xmlFileName = varargin{1};
-    numPoint = 2^varargin{2};
+    xml_file_name = varargin{1};
+    num_fft_pts = 2^varargin{2};
 else
     disp('incorrect number of input arguments');
     return
 end
 
+% Read .xml file
 addpath(genpath(pwd))
 disp('Attempting to parse .xml file...')
-xDoc = parseXML(xmlFileName);
+xml_doc = parseXML(xml_file_name);
 disp('    ...parsing complete.')
 
-numSamples= str2double(xDoc(3).Children( 8).Children.Data);
-sampRate  =	str2double(xDoc(3).Children(10).Children.Data);
-centerFreq= str2double(xDoc(3).Children(22).Children.Children.Children.Children.Data);
-dataType  =	xDoc(3).Children(14).Children.Data;
-fileName  =	xDoc(3).Children(20).Children.Data;
-fID = fopen(fileName);  % Open the binary file
-% scalFact  =	str2double(xDoc(3).Children(16).Children.Data);
-% format    =	xDoc(3).Children(12).Children.Data;
+% Setup program using metadata from .xml file
+num_samples= str2double(xml_doc(3).Children( 8).Children.Data);
+samp_rate  = str2double(xml_doc(3).Children(10).Children.Data);
+center_freq= str2double(xml_doc(3).Children(22).Children.Children.Children.Children.Data);
+data_type  = xml_doc(3).Children(14).Children.Data;
+file_name  = xml_doc(3).Children(20).Children.Data;
+file_ID = fopen(file_name);  % Open the binary file
 
-% duration  = numSamples/sampRate;
-sampPeriod = 1/sampRate;
-t = (0:numPoint-1)*sampPeriod;                       % Time axis array
-f = centerFreq + sampRate/2*linspace(-1,1,numPoint); % Frequency axis array
+samp_period = 1/samp_rate;
+time = (0:num_fft_pts-1)*samp_period;                        % Time axis
+freq = center_freq + samp_rate/2*linspace(-1,1,num_fft_pts); % Freq axis
 
-% Create the figure and define two subplots
-figure('units','normalized','outerposition',[0 0 1 1])
-% ax1 = subplot(2,1,1); % first subplot
-ax2 = subplot(1,1,1); % second subplot
-% title (ax1,'Power vs. Time')
-% xlabel(ax1,'Time (S)')
-% ylabel(ax1,'Power (dBm)')
-% grid  (ax1, 'on');
-% hold(ax1, 'on')
+num_sweeps = floor(num_samples/num_fft_pts);    % Number of plot iterations
+IQ_data_raw = zeros(num_sweeps, num_fft_pts);
+IQ_data_dBm = zeros(num_sweeps, num_fft_pts);
+spectrum = zeros(num_sweeps, num_fft_pts);
 
-i = floor(numSamples/numPoint);    % Number of plot iterations
-IQraw = zeros(i, numPoint);
-IQdBm = zeros(i, numPoint);
-spec = zeros(i, numPoint);
-
-clear fileName numSamples xDoc xmlFileName
+% Read binary file into MatLab
 disp('Loading IQ data into MatLab...')
-for n = 1:i
+for i = 1:num_sweeps
     % Process data from binary file
-    [IQraw(n,:), ~, IQdBm(n,:), ~] = procIQData(fID, numPoint, dataType);
+    [raw,~,dBm,~] = procIQData(file_ID, num_fft_pts, data_type);
+    IQ_data_raw(i,:) = raw;
+    IQ_data_dBm(i,:) = dBm;
     
     % Compute the FFT
-    spec(n,:) = procFFT(IQraw(n,:), numPoint);
+    spectrum(i,:) = procFFT(IQ_data_raw(i,:), num_fft_pts);
 end
 disp('    ...complete.')
-% Set axis limits of the first plot
-% xmin1 = 0;
-% xmax1 = duration;
-% ymin1 = 5*floor(min(IQdBm(:))/5);
-% ymax1 = 5*ceil(max(IQdBm(:))/5);
 
-% Set axis limits of the second plot
-xmin2 = centerFreq - sampRate/2;
-xmax2 = centerFreq + sampRate/2;
-ymin2 = 5*floor(min(spec(:))/5)+10;
-ymax2 = 5*ceil(max(spec(:))/5);
+% Run smaller FFTs to examine chirp properties
+disp('Determining Chirp Rate and Bandwidth...')
+sub_points = 2^6;
+sub_ffts = num_fft_pts/sub_points;
+peak_freq = zeros(1,sub_ffts);
+sub_freq = samp_rate/2 * linspace(-1,1,sub_ffts);
+for i=1:sub_ffts
+    [~,peak_index] = max(procFFT(IQ_data_raw(1,(sub_points*(i-1)+1):sub_points*i), num_fft_pts));
+    peak_freq(1,i) = freq(peak_index);
+end
+sub_spectrum = procFFT(peak_freq(1,:),sub_ffts);
+sub_spectrum(1 + sub_ffts/2) = NaN;
+[~,chirp_ind] = max(sub_spectrum);
+chirp_rate = abs(sub_freq(chirp_ind));
+chirp_rate = prettyUnits(chirp_rate, 'Hz');
+freq_max_chirp = max(peak_freq);
+freq_min_chirp = min(peak_freq);
+band_width = freq_max_chirp - freq_min_chirp;
+band_width = prettyUnits(band_width, 'Hz');
+disp('    ...complete.')
 
-clear centerFreq dataType sampRate
+% Create the plot figure
+figure('units','normalized','outerposition',[0.5 0 0.5 0.5])
 
-for n = 1:i
-%     plot(ax1, t, IQdBm(n,:),'b')     % Plot the time domain plot
-%     axis(ax1, [xmin1 xmax1 ymin1 ymax1])
-    plot(ax2, f, spec(n,:), 'r')     % Plot the FFT plot
-    title (ax2,'Power vs. Frequency')
-    text(xmin2, ymin2+10, ['  Elapsed Time: ', num2str(t(end)), ' s'])
-    xlabel(ax2,'Frequency (Hz)')
-    ylabel(ax2,'Power (dBm)')
-    axis(ax2, [xmin2 xmax2 ymin2 ymax2])
-    grid(ax2, 'on');
+% Set axis limits of the plot
+xmin = center_freq - samp_rate/2;
+xmax = center_freq + samp_rate/2;
+ymin = 5*floor(min(spectrum(:))/5)+10;
+ymax = 5*ceil(max(spectrum(:))/5);
+
+% Create vertical lines to indicate the chirp bandwidth
+max_freq = [freq_max_chirp freq_max_chirp];
+min_freq = [freq_min_chirp freq_min_chirp];
+vert = [ymin ymax];
+
+% Plot FFT output in quick succession to simulate spectrum analyzer
+for i = 1:num_sweeps
+    % Plot FFT and bandwidth markers
+    plot(freq,spectrum(i,:),'r', max_freq,vert,'b', min_freq,vert,'b')
     
-    t = t+(numPoint*sampPeriod);
+    % Title and label plot
+    title ('Power vs. Frequency')
+    xlabel('Frequency (Hz)')
+    ylabel('Power (dBm)')
+    
+    % Display bandwidth, chirp rate, and elapsed time on plot
+    text(xmin, ymin+30, ['  Bandwidth:    ', band_width], 'FontWeight','bold')
+    text(xmin, ymin+20, ['  Chirp Rate:   ', chirp_rate], 'FontWeight','bold')
+    text(xmin, ymin+10, ['  Elapsed Time: ', num2str(time(end)), ' s'], 'FontWeight','bold')
+    
+    % Set x and y axis limits
+    axis([xmin xmax ymin ymax])
+    grid('on');
+    
+    % Increment time array
+    time = time + (num_fft_pts * samp_period);
     pause(.01);
 end
 
-status = fclose(fID);    % Close the binary file
+% Close the binary file
+status = fclose(file_ID);
 if ~status
     disp('File closed:');
-    x = input('Press enter to close the program');
 else
     disp('Error closing file!');
 end
+
+% Pause the program before closing the figure
+input('Press enter to close the program');
 close;
 clear;clc
 end
